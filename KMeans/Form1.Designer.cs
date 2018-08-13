07fd838a44dc78129b0d4d1fbdc5f12794d9660c
lukakostic <41348897+lukakostic@users.noreply.github.com>
2018-08-13 22:06:37 +0200

    Add files via upload

---
+++ KMeans/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace KMeans
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(671, 387);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToResizeColumns = false;
+            this.dataGridView2.AllowUserToResizeRows = false;
+            this.dataGridView2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 405);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(671, 186);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(689, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(198, 43);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Open .csv/.txt to learn from";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "csv";
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Comma separated values | *.csv | Text | .txt";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(738, 110);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 31);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Whats a csv?";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(689, 202);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(198, 43);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Load default training";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(689, 405);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(198, 43);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "<- Predict (Nearest Neighbour)";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(695, 553);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "<-- fill the data and see if it predicts right";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(718, 566);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "it will try to predict the type";
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(689, 454);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(198, 43);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "<- Predict (Clustering)";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 603);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "KMeans";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ KMeans/Form1.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KMeans
+{
+    public partial class Form1 : Form
+    {
+        DataTable dt;
+        DataTable dtTest;
+
+        public List<ItemObject> items;
+
+        public string defaultVals = @"Type,    Sugar, Flour, Water, Yeast
+Cake,     0.8,   0.2,  0.4,     0
+Bread,    0.1,   0.8,  0.5,    0.5
+Doughnut, 0.4,   0.5,  0.5,    0.2
+Cream,    0.9,    0,   0.7,     0
+Pastry,   0.2,   0.2,  0.2,    0.6";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dt = new DataTable("KMeans"); //Top table
+            dtTest = new DataTable("KMeansTest"); //Bottom table
+
+
+
+            dataGridView1.DataSource = dt;
+            dataGridView2.DataSource = dtTest;
+        }
+
+        public void AddColumn(string name, bool isFloat)
+        {
+            if (isFloat)
+            {
+                dt.Columns.Add(name, typeof(float));
+                dtTest.Columns.Add(name, typeof(float));
+            }
+            else
+            {
+                dt.Columns.Add(name, typeof(string));
+                dtTest.Columns.Add(name, typeof(string));
+            }
+        }
+
+        public void AddRow(string s, int rw, int cl)
+        {
+            dt.Rows[rw - 1].SetField(cl, s);
+        }
+
+        private void openFileDialog2_FileOk(object sender, CancelEventArgs e) //Load csv from file
+        {
+            var lines = System.IO.File.ReadAllLines(openFileDialog1.FileName);
+            ParseCSV(ref lines);
+        }
+
+        public void ParseCSV(ref string[] lines)
+        {
+            try
+            {
+
+
+
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    ParseCSVLine(ref lines[i], i);
+                    dt.Rows.Add(dt.NewRow());
+                    //ParseCSVLine(ref lines[i], i-1);
+
+                }
+
+                CleanEmpty();
+                Memorize(); // Assign all elements & their features to a list
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        public void ParseCSVLine(ref string s, int row)
+        {
+            string[] vals = s.Split(',');
+            for (int i = 0; i < vals.Length; i++)
+            {
+                if (row == 0)
+                {
+                    if (i == 0)
+                    {
+                        AddColumn(vals[i], false);
+                    }
+                    else
+                    {
+                        AddColumn(vals[i], true);
+                    }
+                }
+                else
+                {
+
+                    AddRow(vals[i], row, i);
+                }
+            }
+            if (row == 0)
+            {
+                dtTest.Rows.Add(dtTest.NewRow());
+                dtTest.Rows[0].SetField(0, "-------------------------");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) //Whats a csv?
+        {
+            MessageBox.Show(@"A .csv (comma separated values) file
+is just a normal .txt file in similar format:
+
+Type,    Sugar, Flour, Water, Yeast
+Cake,     0.8,   0.2,  0.4,     0
+Bread,    0.1,   0.8,  0.5,    0.5
+Doughnut, 0.4,   0.5,  0.5,    0.2
+Cream,    0.9,    0,   0.7,     0
+Pastry,   0.2,   0.2,  0.2,    0.6
+
+First (0th) row is labels for data,
+its not looked at by the program.
+
+First column must be a string (text)
+First column is type or classification
+others are floats or ints (numbers)
+
+you dont have to line them up
+");
+        }
+
+        void CleanEmpty() //Delete empty table rows
+        {
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                if (dt.Rows[i].ItemArray[0].ToString() == "") dt.Rows[i].Delete();
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) //Load from file button
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var lines = defaultVals.Split(Environment.NewLine.ToCharArray());
+            ParseCSV(ref lines);
+        }
+
+        private void button4_Click(object sender, EventArgs e) //Predict nearest neighbour button
+        {
+            try
+            {
+                KNearestNeighbour();
+                MessageBox.Show("Done. Check the type(s) in bottom table");
+            }
+            catch (Exception ex) { MessageBox.Show("Error. Check if you filled all bottom fields"); }
+        }
+
+        public void KNearestNeighbour()
+        {
+            for (int bi = 0; bi < dtTest.Rows.Count; bi++)
+            {
+
+
+                var features = new float[dtTest.Rows[bi].ItemArray.Length - 1];
+                var distances = new float[items.Count];
+                for (int i = 1; i < dtTest.Rows[bi].ItemArray.Length; i++)
+                {
+                    features[i - 1] = (float)dtTest.Rows[bi].ItemArray[i];
+                }
+
+                for (int a = 0; a < items.Count; a++)
+                {
+                    //////////////////////////////////////////////////////////////////////Core of K-Nearest Neighbour: You just find the closest element, features are coordinates
+                    var dist = 0f;
+                    for (int i = 0; i < features.Length; i++)
+                    {
+                        dist += (features[i] - items[a].features[i]) * (features[i] - items[a].features[i]); // Square distance for each feature
+                    }
+                    distances[a] = (float)Math.Sqrt((double)dist); //Pythagoras to calc n-dimensional distance
+                }
+
+                int minI = 0;
+                float minDist = float.MaxValue;
+                for (int i = 0; i < distances.Length; i++)
+                {
+                    if (distances[i] < minDist)
+                    {
+                        minI = i;
+                        minDist = distances[i];
+                    }
+                }
+
+                dtTest.Rows[bi].SetField(0, items[minI].typeName);
+            }
+        }
+
+        public void KMeansClustering()
+        {
+            var clusters = new List<ItemObject>(); //Instead of comparing to each element, we compare to center of cluster of each type
+            var itemsPerCluster = new List<int>();
+
+            for (int a = 0; a < items.Count; a++)
+            {
+                bool added = false;
+                for (int b = 0; b < clusters.Count; b++)
+                {
+                    if (clusters[b].typeName == items[a].typeName)
+                    {
+                        for (int i = 0; i < items[a].features.Length; i++)
+                        {
+                            clusters[b].features[i] += items[a].features[i];
+                        }
+
+                        itemsPerCluster[b] += 1;
+                        added = true;
+                        break;
+                    }
+                }
+                if (!added)
+                {
+                    var i = new ItemObject();
+                    i.typeName = items[a].typeName;
+                    i.features = items[a].features;
+                    clusters.Add(i);
+                    itemsPerCluster.Add(1);
+                }
+            }
+            for (int b = 0; b < clusters.Count; b++) //Average the features for each cluster to get the center
+            {
+                for (int i = 0; i < clusters[b].features.Length; i++)
+                {
+                    clusters[b].features[i] /= (float)itemsPerCluster[b];
+                }
+            }
+
+            for (int bi = 0; bi < dtTest.Rows.Count; bi++)
+            {
+
+                var features = new float[dtTest.Rows[bi].ItemArray.Length - 1];
+                for (int i = 1; i < dtTest.Rows[bi].ItemArray.Length; i++)
+                {
+                    features[i - 1] = (float)dtTest.Rows[bi].ItemArray[i];
+                }
+                var distances = new float[clusters.Count];
+
+
+                for (int a = 0; a < clusters.Count; a++)
+                {
+                    //////////////////////////////////////////////////////////////////////Core of K-Means Clustering: You just find the closest cluster, features are coordinates
+                    var dist = 0f;
+                    for (int i = 0; i < features.Length; i++)
+                    {
+                        dist += (features[i] - clusters[a].features[i]) * (features[i] - clusters[a].features[i]); // Square distance for each feature
+                    }
+                    distances[a] = (float)Math.Sqrt((double)dist); //Pythagoras to calc n-dimensional distance
+                }
+
+                int minI = 0;
+                float minDist = float.MaxValue;
+                for (int i = 0; i < distances.Length; i++)
+                {
+                    if (distances[i] < minDist)
+                    {
+                        minI = i;
+                        minDist = distances[i];
+                    }
+                }
+
+                dtTest.Rows[bi].SetField(0, clusters[minI].typeName);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                KMeansClustering();
+                MessageBox.Show("Done. Check the type(s) in bottom table");
+            }
+            catch (Exception ex) { MessageBox.Show("Error. Check if you filled all bottom fields"); }
+        }
+
+        public void Memorize() //Read elements & features and add them to the list
+        {
+            items = new List<ItemObject>();
+
+            for (int a = 0; a < dt.Rows.Count; a++)
+            {
+                var it = new ItemObject();
+                it.typeName = (string)dt.Rows[a].ItemArray[0];
+
+                it.features = new float[dt.Rows[a].ItemArray.Length - 1];
+
+                for (int i = 1; i < dt.Rows[a].ItemArray.Length; i++)
+                {
+                    it.features[i - 1] = (float)dt.Rows[a].ItemArray[i];
+                }
+
+                items.Add(it);
+
+            }
+
+
+        }
+
+
+
+        public class ItemObject
+        {
+            public string typeName;
+            public float[] features;
+        }
+
+    }
+}
